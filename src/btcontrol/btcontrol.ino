b41c57ac2929343d0b7702f3c7ab2030293b6e20
Rafael Sotomayor Brulé <rsotomayor@savtec.cl>
2016-04-09 17:41:26 -0300

    Corrige y ordena código

--- src/btcontrol/btcontrol.ino
+++ src/btcontrol/btcontrol.ino
@@ -1,21 +1,25 @@
 #include <SoftwareSerial.h>
 
-int digPinPWM = 10; // pin digital 10 Para PM
-
-// Optoacopladores en las entradas digitales D4, D5, D6 y D7
-int Opto1 = 7;     
-int Opto2 = 6;
-int Opto3 = 5;
-int Opto4 = 4;
+// Software serial #1: RX = digital pin 8, TX = digital pin 9
+#define PIN_TX 9
+#define PIN_RX 8
 
 // Las entradas análogas se comportan como salidas digitales A0, A1, A2 y A3
-int Relay1 =  A0;   
-int Relay2 =  A1;
-int Relay3 =  A2;
-int Relay4 =  A3;
+#define RELAY1  A0
+#define RELAY2  A1
+#define RELAY3  A2
+#define RELAY4  A3
 
-// Software serial #1: RX = digital pin 8, TX = digital pin 9
-SoftwareSerial portOne(8, 9);
+// Optoacopladores en las entradas digitales D4, D5, D6 y D7
+#define OPTO1   7
+#define OPTO2   6
+#define OPTO3   5
+#define OPTO4   4
+
+#define PIN_PWM 10
+
+SoftwareSerial portOne(PIN_RX, PIN_TX);
+
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -25,16 +29,27 @@ void setup() {
   }
 
 // configurando los pines como salidas digitales
-  pinMode(Relay1, OUTPUT);  
-  pinMode(Relay2, OUTPUT);
-  pinMode(Relay3, OUTPUT);
-  pinMode(Relay4, OUTPUT);
+  pinMode(RELAY1, OUTPUT);  
+  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAY3, OUTPUT);
+  pinMode(RELAY4, OUTPUT);
   
 // configurando los pines como entradas digitales
-  pinMode(Opto1, INPUT);  
-  pinMode(Opto2, INPUT); 
-  pinMode(Opto3, INPUT); 
-  pinMode(Opto4, INPUT); 
+  pinMode(OPTO1, INPUT);  
+  pinMode(OPTO2, INPUT); 
+  pinMode(OPTO3, INPUT); 
+  pinMode(OPTO4, INPUT);
+ 
+//Obs: Si tienes la versión de la placa 1.06, para que funcione correctamente debes configurar una resistencia
+// Pull-UP en el Pin de Salida (TX) del Arduino (RX del Bluetooth), ya que esta versión cuenta con un Diodo,
+// el cual se debe polarizar para la transmisión de datos.
+// En el bloque de código setup() debes configurar una resistencia Pull-Up (Pin TX de Arduino en estado alto,
+// HIGH) de la siguiente manera. Pin 15 Arduino Mega, o Pin 0 Arduino UNO (Depende del puerto serial de 
+// Arduino mega que estén usando).
+
+//  pinMode(PIN_TX, INPUT);
+//  digitalWrite(PIN_TX, HIGH); 
+  
   portOne.begin(9600);
 }
 
@@ -45,7 +60,6 @@ String strResponse;
 int    counterCRLF=0;
 bool   flagRxCommand=false ;
 bool   flagCommandValido ;
-
 
 void loop() {
   
@@ -72,46 +86,44 @@ void loop() {
     strCommand = strRx.substring(0,(strRx.length()-2));
     strCommand.toUpperCase();
 
-
     flagRxCommand     = false ;
     strRx             = "";
     flagCommandValido = true ;
     
     if ( strCommand == "R1ON" ) {
-      digitalWrite(Relay1, HIGH); 
+      digitalWrite(RELAY1, HIGH); 
     } else if ( strCommand == "R2ON" ) {
-      digitalWrite(Relay2, HIGH); 
+      digitalWrite(RELAY2, HIGH); 
     } else if ( strCommand == "R3ON" ) {
-      digitalWrite(Relay3, HIGH); 
+      digitalWrite(RELAY3, HIGH); 
     } else if ( strCommand == "R4ON" ) {
-      digitalWrite(Relay4, HIGH); 
+      digitalWrite(RELAY4, HIGH); 
     } else if ( strCommand == "R1OFF" ) {
-      digitalWrite(Relay1, LOW); 
+      digitalWrite(RELAY1, LOW); 
     } else if ( strCommand == "R2OFF" ) {
-      digitalWrite(Relay2, LOW); 
+      digitalWrite(RELAY2, LOW); 
     } else if ( strCommand == "R3OFF" ) {
-      digitalWrite(Relay3, LOW); 
+      digitalWrite(RELAY3, LOW); 
     } else if ( strCommand == "R4OFF" ) {
-      digitalWrite(Relay4, LOW); 
+      digitalWrite(RELAY4, LOW); 
     } else if ( strCommand == "S0" ) {
-      digitalWrite(Relay4, LOW); 
-      analogWrite(digPinPWM,0); // Señal PWM a 0% en el PIN
+      analogWrite(PIN_PWM,0); // Señal PWM a 0% en el PIN
     } else if ( strCommand == "S25" ) {
-      analogWrite(digPinPWM,63); // Señal PWM a 25% en el PIN
+      analogWrite(PIN_PWM,63); // Señal PWM a 25% en el PIN
     } else if ( strCommand == "S50" ) {
-      analogWrite(digPinPWM,127); // Señal PWM a 50% en el PIN
+      analogWrite(PIN_PWM,127); // Señal PWM a 50% en el PIN
     } else if ( strCommand == "S75" ) {
-      analogWrite(digPinPWM,190); // Señal PWM a 75% en el PIN
+      analogWrite(PIN_PWM,190); // Señal PWM a 75% en el PIN
     } else if ( strCommand == "S100" ) {
-      analogWrite(digPinPWM,255); // Señal PWM a 100% en el PIN
+      analogWrite(PIN_PWM,255); // Señal PWM a 100% en el PIN
     } else {
       flagCommandValido = false ;
     }
     
     if ( flagCommandValido ) {
-      strResponse = strCommand + " -> OK\r\n";
+      strResponse = strCommand + " => OK\r\n";
     } else {
-      strResponse = strCommand + " -> ERROR\r\n";
+      strResponse = strCommand + " => ERROR\r\n";
     }
     Serial.print(strResponse);      
     portOne.write(strResponse.c_str());
